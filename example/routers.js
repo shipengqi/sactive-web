11cde4f5aefca736bce22ca59804f0115eba06fe
Shi <peng-qi.shi@hpe.com>
2018-06-08 13:25:08 +0800

    add example

--- example/routers.js
+++ example/routers.js
@@ -1,7 +1,14 @@
-module.exports = [{
+const AsctiveWeb = require('../lib/application');
+let example1 = {
+  name: 'example1',
   method: 'get',
   path: '/example',
   handler: () => {
     console.log('example router');
   }
-}];
+};
+
+let app = new AsctiveWeb();
+app.route(example1);
+app.init();
+app.listen('9000');
