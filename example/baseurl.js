a84d185cf249940c578e466422f34a3bc4c9ed99
Shi <peng-qi.shi@hpe.com>
2018-06-11 10:19:46 +0800

    add base url option

---
+++ example/baseurl.js
@@ -0,0 +1,17 @@
+const AsctiveWeb = require('../lib/application');
+
+let example1 = {
+  name: 'hello',
+  method: 'get',
+  path: '/example/hello',
+  handler: (ctx, next) => {
+    ctx.body = 'Hello SActive !!!';
+  }
+};
+
+let app = new AsctiveWeb({baseUrl: '/api/test'});
+app.route(example1);
+app.load(`${__dirname}/routes`);
+
+app.init();
+app.listen(9000);
